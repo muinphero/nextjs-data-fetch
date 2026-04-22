a51af9c55c91540e2a86e9b87e0a69a0bb7dedc9
Muin Chowdhury <muin.bd@gmail.com>
2026-04-22 14:08:11 +0600

    json server added

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,7 +1,25 @@
-const PostsPage = () => {
+const PostsPage = async () => {
+  const getPosts = async () => {
+    // try {
+    //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //   const data = await res.json();
+    //   return data;
+    // } catch (error) {
+    //   throw new Error("Failed to fetch posts");
+    // }
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    return res.json();
+  };
+
+  const posts = await getPosts();
+
   return (
     <div>
-      <h2>Posts are coming soon...</h2>
+      <h2>Posts are coming soon..{posts.length}</h2>
     </div>
   );
 };
